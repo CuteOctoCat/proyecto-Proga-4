2fcb9490014e9fe284babd185d198959cc5f62e5
Admin <Admin@192.168.1.14>
2016-10-02 11:13:08 -0700

    Agregando login modal

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -5,6 +5,13 @@ $(function () {
         $("#myModalFormulario").appendTo("body").modal("show");
     });
 
+    $("#btMostarIngresar").click(function () {
+        $("#myModalIngresar").appendTo("body").modal("show");
+    });
+
+    $("#btMostarForm1").click(function () {
+        $('#myModalFormulario').appendTo("body").modal("show");
+    });
 
 });
 
